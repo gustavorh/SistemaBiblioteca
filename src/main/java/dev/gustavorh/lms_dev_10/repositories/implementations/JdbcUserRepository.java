607565cd955299bda4f3e7d0970be11ab1e728d3
Gustavo Reyes <gustavo.reyes@tutanota.com>
2024-11-20 00:23:17 -0300

    Stable 4.2 - Fix bugs

--- src/main/java/dev/gustavorh/lms_dev_10/repositories/implementations/JdbcUserRepository.java
+++ src/main/java/dev/gustavorh/lms_dev_10/repositories/implementations/JdbcUserRepository.java
@@ -56,7 +56,6 @@ public class JdbcUserRepository implements IUserRepository {
                 throw new SQLException("Updating user role failed, no rows affected.");
             }
         }
-
     }
 
     @Override
